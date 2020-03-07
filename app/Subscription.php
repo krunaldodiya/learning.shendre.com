34c22288f47268cba548071bed5eae42ab7af8dd
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-03-07 19:35:09 +0530

    added admin

---
+++ app/Subscription.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use App\Traits\HasUuid;
+use Illuminate\Database\Eloquent\Model;
+
+class Subscription extends Model
+{
+    use HasUuid;
+
+    protected $guarded = [];
+
+    protected $dates = [
+        'created_at', 'updated_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function plan()
+    {
+        return $this->belongsTo(Plan::class);
+    }
+}
---
+++ database/migrations/2020_03_07_140252_create_subscriptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->uuid('plan_id');
+            $table->foreign('plan_id')->references('id')->on('plans')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
