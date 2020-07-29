ff9277c66a2d03774f2a7153d0a16e05094c622a
kunal dodiya <kunal.dodiya1@gmail.com>
2020-07-29 11:01:52 +0530

    done

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Notification;
+
 class NotificationController extends Controller
 {
     public function updateProfile(UpdateProfile $request)
