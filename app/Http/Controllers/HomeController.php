27336ad8dde0b01e0b79f02185c0f5bec100f7a2
kunal dodiya <kunal.dodiya1@gmail.com>
2020-08-02 10:49:40 +0530

    done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Storage;
+
 class HomeController extends Controller
 {
     public function getMediaFile(Request $request)
     {
         $media = $request->media;
+
         $file = $request->file;
 
-        if ($file) {
+        if (Storage::disk('public')->exists($file)) {
             return response()->file("storage/{$file}");
         }
 
